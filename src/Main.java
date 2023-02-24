26ccc4434e54c406930021aaa9ff03d8fb86bcac
christopherBryanSMC <109288260+christopherBryanSMC@users.noreply.github.com>
2023-02-23 22:28:51 -0600

    version 1.0.1

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,11 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        String[] numberOptionA= {"0", "0.5", "1", "0", "-1", "-0.5", "0.707", "-0.707", "0.666", "-0.666", "0.866", "-0.866"};
-        int[][] numberOptionb ={{2, 3}, {3, 3}, {4, 5}, {5, 2}};
+        String [] numberOptionA= {"0", "0.5", "1", "0", "-1", "-0.5", "0.707", "-0.707", "0.666", "-0.666", "0.866", "-0.866"};
+        int [][] numberOptionb ={{2, 3}, {3, 3}, {4, 5}, {5, 2}};
+        float [][] numberOptionC={{2.5f,3.2f}, {3f,3f}, {4.146f,5.6325f}, {5.7f,2.523f}};
+        String [][] numberOptionD={{"2","3"}, {"3","3"}, {"4","5"}, {"5","2"}};
+
         ArtekMath artekMath = new ArtekMath();
 
         System.out.println("\nActividad 1\n");
@@ -18,10 +21,75 @@ public class Main {
         for (String a: numberOptionA){
             System.out.println(artekMath.GetSin( a ));
         }
-        //Prueba con A
+        //Prueba con B
+        System.out.println("Prueba con B");
         for (int[] b:numberOptionb){
             System.out.println(artekMath.GetSin( b[0],b[1] ));
         }
+
+        //Prueba con C
+        System.out.println("Prueba con C");
+        for (float[] c:numberOptionC){
+            System.out.println(artekMath.GetSin( c[0],c[1] ));
+        }
+
+        //Prueba con D
+        System.out.println("Prueba con D");
+        for (String[] d:numberOptionD){
+            System.out.println(artekMath.GetSin( d[0],d[1] ));
+        }
+
+
+        System.out.println("\nActividad 2\n");
+
+        //Prueba con A
+        System.out.println("Prueba con A");
+        for (String a: numberOptionA){
+            System.out.println(artekMath.GetCos( a ));
+        }
+        //Prueba con B
+        System.out.println("Prueba con B");
+        for (int[] b:numberOptionb){
+            System.out.println(artekMath.GetCos( b[0],b[1] ));
+        }
+
+        //Prueba con C
+        System.out.println("Prueba con C");
+        for (float[] c:numberOptionC){
+            System.out.println(artekMath.GetCos( c[0],c[1] ));
+        }
+
+        //Prueba con D
+        System.out.println("Prueba con D");
+        for (String[] d:numberOptionD){
+            System.out.println(artekMath.GetCos( d[0],d[1] ));
+        }
+
+        System.out.println("\nActividad 3\n");
+
+        //Prueba con A
+        System.out.println("Prueba con A");
+        for (String a: numberOptionA){
+            System.out.println(artekMath.GetTan( a ));
+        }
+        //Prueba con B
+        System.out.println("Prueba con B");
+        for (int[] b:numberOptionb){
+            System.out.println(artekMath.GetTan( b[0],b[1] ));
+        }
+
+        //Prueba con C
+        System.out.println("Prueba con C");
+        for (float[] c:numberOptionC){
+            System.out.println(artekMath.GetTan( c[0],c[1] ));
+        }
+
+        //Prueba con D
+        System.out.println("Prueba con D");
+        for (String[] d:numberOptionD){
+            System.out.println(artekMath.GetTan( d[0],d[1] ));
+        }
+
 
         System.out.println("\nActividad 4\n");
         String test = "Juan,Pedro,Maria,Jose,Abad,Obed,Bryan,Miguel";
